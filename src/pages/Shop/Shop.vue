86c3d4a5d121fc1761275f6a169985e4784c4437
DESKTOP-O80MIEU\ThePC <945969978@qq.com>
2021-09-08 09:24:38 +0800

    init gshop

---
+++ src/pages/Shop/Shop.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Shop
+  </div>
+</template>
+
+<script>
+    export default {
+    name: 'Shop'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
